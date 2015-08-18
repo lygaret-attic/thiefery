834052b0fddfbe96a4060bfbb8e7379f99249aa8
Jonathan Raphaelson <jon@accidental.cc>
2015-08-17 23:04:21 -0600

    getting refamiliar, added a lose state

--- src/thiefery/core.clj
+++ src/thiefery/core.clj
@@ -5,11 +5,14 @@
 (defmethod state/draw :boot [state scr]
   (screen/put-string scr 10 10 "Welcome to THIEFERY.")
   (screen/put-string scr 10 11 "A game of thiefing, adventure and glory.")
-  (screen/put-string scr 10 12 "Press any key to win."))
+  (screen/put-string scr 10 12 "Press enter to win."))
 
 (defmethod state/run :boot [state scr stack]
-  (screen/get-key-blocking scr)
-  (conj stack (state/make-state :win {:how "By being awesome!"})))
+  (let* [key (screen/get-key-blocking scr)
+         next (if (= key :enter)
+                (state/make-state :win {:how "By being awesome!"})
+                (state/make-state :lose {:how "By not following instructions."}))]
+        (conj stack next)))
 
 (defmethod state/draw :win [state scr]
   (screen/put-string scr 10 10 "You won THIEFERY!")
@@ -20,13 +23,29 @@
   (screen/get-key-blocking scr)
   stack)
 
-(defn- game-loop [scr boot]
+(defmethod state/draw :lose [state scr]
+  (screen/put-string scr 10 10 "You lost THIEFERY!")
+  (screen/put-string scr 10 11 (get-in state [:params :how]))
+  (screen/put-string scr 10 12 "Press any key to exit."))
+
+(defmethod state/run :lose [state scr stack]
+  (screen/get-key-blocking scr)
+  stack)
+
+(defn- game-cycle
+  "Run one iteration of the game."
+  [scr stack]
+  (screen/clear scr)
+  (doseq [x stack] (state/draw x scr))
+  (screen/redraw scr)
+  (state/run (peek stack) scr (pop stack)))
+
+(defn- game-loop
+  "Run the game to completion."
+  [scr boot]
   (loop [stack boot]
     (when (seq stack)
-      (screen/clear scr)
-      (doseq [x stack] (state/draw x scr))
-      (screen/redraw scr)
-      (recur (state/run (peek stack) scr (pop stack))))))
+      (recur (game-cycle scr stack)))))
 
 (defn- main [type]
   (let [scr (screen/get-screen type)]
--- src/thiefery/state.clj
+++ src/thiefery/state.clj
@@ -1,9 +1,9 @@
 (ns thiefery.state
   (:require [lanterna.screen :as screen]))
 
-(defn make-state 
+(defn make-state
   ([name]
-    (make-state name {}))
+   (make-state name {}))
   ([name params]
    {:name name :params params}))
 
@@ -12,20 +12,19 @@
   (fn [state scr]
     (:name state)))
 
-(defmulti run 
-  "Executes whatever code takes place at this state, side effects 
-   and input, and returns a new state stack."
+(defmulti run
+  "Executes whatever code takes place at this state, side effects and input, and returns a new state stack."
   (fn [state scr stack]
     (:name state)))
 
-(defmethod draw :default 
+(defmethod draw :default
   [state scr]
   (do
     (screen/put-string scr 10 9  "!!!")
     (screen/put-string scr 10 10 (str "Unknown state: " state))
     (screen/put-string scr 10 10 "!!!")))
 
-(defmethod run :default 
-  [state scr stack] 
+(defmethod run :default
+  [state scr stack]
   (screen/get-key-blocking scr)
   [])
